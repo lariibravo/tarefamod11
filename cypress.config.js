b6777ecc029d8f21e26df73665973045d9e27979
Larissa Bravo <lahbravo@hotmail.com>
2022-09-27 15:32:36 -0300

    relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'gxff4w',
   e2e: {
     baseUrl : 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
